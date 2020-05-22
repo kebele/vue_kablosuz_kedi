c3604809c7360f98278c3e36536d96088a1062f2
kebele <ykemalbeler@gmail.com>
2020-05-23 02:42:30 +0300

    degisiklik

--- BOLUM 2/odev-6/src/App.vue
+++ BOLUM 2/odev-6/src/App.vue
@@ -12,8 +12,8 @@
 </template>
 
 <script>
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
 import Servers from './components/Servers';
 import ServerDetails from './components/ServerDetails'
 export default {
