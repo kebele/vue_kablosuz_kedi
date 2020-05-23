8b63446bcf1db3dae09c55e8806d3fd344279826
kebele <ykemalbeler@gmail.com>
2020-05-23 13:56:00 +0300

    ekleme

--- BOLUM 2/component-iletisim/src/components/User.vue
+++ BOLUM 2/component-iletisim/src/components/User.vue
@@ -2,34 +2,61 @@
   <div class="container">
     <h1>Parent Component (User)</h1>
     <p>Burası parent component yani herşeyin import edildiği component :)</p>
-    <hr>
+    <button @click="changeName">adımı değiştir</button>
+    <hr />
     <div class="row">
-      <app-user-detail></app-user-detail>
+      <!-- <app-user-detail name="rafael varane"></app-user-detail> -->
+      <app-user-detail :name="title"></app-user-detail>
       <app-user-edit></app-user-edit>
     </div>
   </div>
 </template>
 <script>
-  import UserDetail from "./UserDetail";
-  import UserEdit from "./UserEdit";
-  export default {
-    components : {
-      appUserDetail : UserDetail,
-      appUserEdit : UserEdit,
-    }
-  }
+import UserDetail from "./UserDetail";
+import UserEdit from "./UserEdit";
+export default {
+  components: {
+    appUserDetail: UserDetail,
+    appUserEdit: UserEdit,
+  },
+  data() {
+    return {
+      title: "ronaldo",
+    };
+  },
+  methods: {
+    changeName() {
+      this.title = "sergio";
+    },
+  },
+};
 </script>
 
 <style>
-  div.container{
-    margin-top: 30px;
-    padding: 20px 40px;
-    background-color: #6a8d99;
-    border: 1px solid #666;
-  }
-  div.row {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
+div.container {
+  margin-top: 30px;
+  padding: 20px 40px;
+  background-color: #6a8d99;
+  border: 1px solid #666;
+}
+div.row {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 </style>
+
+/* istediğimiz şu butona tıkladığımızda title değerinin UserDatei.vue daki name
+e gitmesi, yani parent tan child a data transferi şimdi burada yapacağımız şey;
+UserDetail.vue nun component adı appUserDetail, bunu da
+<app-user-detail name="rafael varane"></app-user-detail>
+şeklinde kullandık, içine name="" yazdık, işte bu name bu componente gidecek
+demek, zaten UserDetail.vue içinde de name yakalayacağımızı belirlemiştik.
+name="rafael varane" yi UserDetail.vue yakaladı ikinci durumda eğer buradaki
+data daki title ı göndermek istiyorsak name i title a bind etmemiz lazım, yani
+:name="title" şeklinde bunu yaptığımızda bu sefer de ronaldo yu yollamış oluruz
+burada butona basarsak title değeri değişecek sergio olacak bu durumda kullanıcı adı da sergio şeklinde değişmiş olacak
+
+burada yaptığımız şey
+PARENT to CHILD buna dikkat
+*/
--- BOLUM 2/component-iletisim/src/components/UserDetail.vue
+++ BOLUM 2/component-iletisim/src/components/UserDetail.vue
@@ -2,9 +2,14 @@
   <div class="wcol-md-6">
     <h3>Child Component 1</h3>
     <p>Ben User.vue isimli Parent Component'in içerisindeki bir Child componentim</p>
+    <p>kullanıcı adı : {{ name }}</p>
   </div>
 </template>
-<script></script>
+<script>
+export default {
+  props : ["name"]
+}
+  </script>
 <style scoped>
   div {
     background-color: lightcoral;
@@ -13,3 +18,14 @@
     display: inline-block;
   }
 </style>
+
+/* 
+dışarıdan gelecek dataları burada props ile alırız
+props : ["","","",""]
+props her zaman bir arraydir, bu şekilde array içinde string şeklinde tanımlanır
+bizim burada dışarıdan almak istediğimiz veri name
+bunu burada tanımladığımızda/hazırladığımızda artık burada sanki data(){} içerisinde tanımlanmış gibi davranmaya başlar
+artık dışarıdan name isimli bir veriyi kabul etmeye başladı
+şimdi bu veriyi parent tan yani User.vue dan alacak, oraya gidip bunu dışarıya gönderelim
+
+ */
