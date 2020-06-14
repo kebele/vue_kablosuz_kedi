1fa5ffcb9b262c3cfdb04b2fc6c5423ce550cf2e
kebele <ykemalbeler@gmail.com>
2020-06-15 01:43:56 +0300

    deg

--- BOLUM 2/intro/src/App.vue
+++ BOLUM 2/intro/src/App.vue
@@ -7,12 +7,13 @@
 
 <script>
 /* 
+//COMPONENT YAPISI BOLUMUNDE 2. KISIM
   burada
   data : 
   component : 
   watch: computed :
   bir vue instance ı içinde ne varsa burada tanımlayabiliriz
-  uygulamamnın busines logic burada yapılır,
+  uygulamamnın busines logic i burada yapılır,
   ancak eğer burada bir static olacaksa bu dosyada yani bir logic vb. bir şey yoksa buraya bir şey tanımlamamıza ihtiyac yok
   data : tanımlanması biraz farklı sadece
   style kısmıda aynı şekilde eğer ihtiyacımız yoksa kalkabilir
