3604785e71e1628ca01d447547605f45f262deb9
kebele <ykemalbeler@gmail.com>
2020-06-15 12:49:56 +0300

    deg

--- BOLUM 2/intro/src/App.vue
+++ BOLUM 2/intro/src/App.vue
@@ -8,10 +8,14 @@
 <script>
 /* 
 //COMPONENT YAPISI BOLUMUNDE 2. KISIM
+
+App.vue bizim ana (root) componentimiz
+
   burada
   data : 
   component : 
-  watch: computed :
+  watch: 
+  computed :
   bir vue instance ı içinde ne varsa burada tanımlayabiliriz
   uygulamamnın busines logic i burada yapılır,
   ancak eğer burada bir static olacaksa bu dosyada yani bir logic vb. bir şey yoksa buraya bir şey tanımlamamıza ihtiyac yok
@@ -21,10 +25,26 @@
 
   şimdi data ile işlem yapalım biraz
 
-içinde old. dosya App.vue bizim root componentimizdir
+içinde olduğumuz dosya App.vue bizim root componentimizdir
 
 yeni bir component oluşturalım
 
+bir componentte 
+template mutlaka olacak
+script eğer bir logic kullanacaksak js kodu olacaksa (kod template içinede yazılabilr) olmalı ama şart değil
+style bu da eğer stil işlemi olacaksa olmalı yoksa bu da şart değil
+
+<lamp-component /> kullandık, demek ki böyle bir component var veya yapacağız
+
+bu main js e koyduk orada da dedik ki Lamp-component yap ve bunun verisini Home.vue dan al
+
+şimdi bir lampStatus.vue yaptık, bunun içinde lambanun durumu ve lambayı açacak buton var, bu lambanın bulunduğu satırı verecek
+
+bunu export edeceğiz data içinde lambanın status unu tanımladık default kapalı
+ayrıca methods içinde de bu status u değiştrecek methodu koyduk
+bunu export ettik yani artık kullamka istediğimiz yerde import ederek kullanabiliriz, şu anda herhangibir parametre almadıklarına dikkat
+
+Home.vue da LampStatus.vue yu import ettik ve components içindede tanımladık, bu da burada ben bunu comp olarak kullanacağım demek ve bunu template içine yazdık, içine 5 kez aynı işlemi yapsın diye v-for yazdık
 
   */
 export default {
