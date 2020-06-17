07df3919cf754f30ba347854d660992e26dd5268
kebele <ykemalbeler@gmail.com>
2020-06-17 18:29:22 +0300

    ekleme

--- BOLUM 2/gelismis component kull/src/App.vue
+++ BOLUM 2/gelismis component kull/src/App.vue
@@ -2,18 +2,26 @@
   <div class="container">
     <div class="row">
       <div class="col-md-6 col-md-offset-3">
-        <app-memory></app-memory>
+        <!-- <app-memory content="bu bir anıdır"></app-memory> -->
+        <app-memory>
+          <h1 slot="heading">güzel bir gün</h1>
+          <p slot="content">bu süper bir anıydı</p>
+        </app-memory>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import Memory from "./Memory";
+  import Memory from "./components/Memory";
+  import NewMemory from "./components/NewMemory";
+  import Author from "./components/Author";
 
   export default {
     components : {
-      appMemory : Memory
+      appMemory : Memory,
+      appNewMemory : NewMemory,
+      appAuthor : Author,
     }
   }
 </script>
---
+++ BOLUM 2/gelismis component kull/src/components/Memory.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <!-- <p class="text-center"> Bu çok güzel bir anıdır...</p> -->
+    <!-- eğer kapsamlı bir html göndermek istersek     
+    <p class="text-center">{{ content }}</p> 
+    -->
+    <div class="memory">
+      <slot name="heading"></slot>
+    </div>
+    <div class="memory">
+      <slot name="content"></slot>
+    </div>
+    <div class="memory">
+      <slot name="author">
+        <h3>yazar</h3>
+        <p>buraya herhangibir içerik gelmemiş</p>
+      </slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  // props : ["content"]
+};
+</script>
+<style scoped>
+/* scoped sadece burada geçerli olsun */
+div.memory {
+  border: 1px solid #ccc;
+  box-shadow: 1px 1px 2px #666;
+  padding: 20px 20px;
+  margin-top: 20px;
+}
+</style>
