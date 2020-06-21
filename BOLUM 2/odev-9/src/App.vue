659edec8d49d15a0a7dcba50da4cad17c79ec501
kebele <ykemalbeler@gmail.com>
2020-06-21 07:34:42 +0300

    ekleme

---
+++ BOLUM 2/odev-9/src/App.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="container">
+        <form v-if="!isOn">
+            <div class="row" >
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <!-- Soru 1 -->
+                    <!-- Aşağıdaki bilgileri içeren bir Üye Ol sayfası geliştiriniz. -->
+                    <!-- Tam Adı (Ad + Soyad) -->
+                    <!-- E-mail -->
+                    <!-- Şifre -->
+                    <!-- Cinsiyet (Kadın/Erkek) -->
+
+                    <!-- FullName.vue ya taşıdım -->
+                    <!-- <div class="form-group">
+                        <label for="">adı</label>
+                        <input type="text" class="form-control" v-model="userData.name">
+                    </div>
+                    <div class="form-group">
+                        <label for="">soyadı</label>
+                        <input type="text" class="form-control" v-model="userData.surname">
+                    </div> -->
+                    
+                    <app-fullname v-model="fullname"></app-fullname>
+
+                    <div class="form-group">
+                        <label for="">e-mail</label>
+                        <input type="email" class="form-control" v-model="userData.email">
+                    </div>
+                    <div class="form-group">
+                        <label for="">sifre</label>
+                        <input type="password" class="form-control" v-model="userData.password">
+                    </div>
+                    <div class="form-group">                        
+                        <label for="">
+                            <input type="radio" v-model="userData.gender" value="kadın"> kadın
+                        </label>
+                        <label for="">
+                            <input type="radio" v-model="userData.gender" value="erkek"> erkek
+                        </label>
+                    </div>
+                    <button @click.prevent="gonder" class="btn btn-primary">gönder</button>
+
+
+                    <!-- Soru 2 -->
+                    <!-- Üye Ol formu, Submit EDİLMEDİĞİ SÜRECE ekranda gözükmelidir.-->
+                    <!-- Üye Bilgileri Ekranı ise form Submit EDİLDİKTEN SONRA ekranda gözükecektir -->
+
+                    <!-- Soru 3 -->
+                    <!-- Yukarıdaki uygulamayı düzenleyerek Tam Ad (Ad + Soyad) alanı için bir Form Kontrol Elemanı üretiniz -->
+
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div v-if="isOn" class="row" >
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-info">
+                    <div class="panel-heading">
+                        <h4>Girdiğiniz Bilgier</h4>
+                    </div>
+                    <div class="panel-body">
+                        <!-- <p>Tam Ad: {{userData.name}} {{userData.surname}}</p> -->
+                        <p>Tam Ad: {{userData.fullname}}</p>
+                        <p>E-mail: {{userData.email}}</p>
+                        <p>Şifre: {{userData.password}}</p>
+                        <p>Cinsiyet: {{userData.gender}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Fullname from './Fullname'
+
+    export default {
+        components : {
+            appFullname : Fullname
+        },
+        data(){
+            return {
+                userData : {
+                    name : '',
+                    surname : '',
+                    email : '',
+                    password : '',
+                    gender : '',                    
+                },
+                isOn : false,
+                fullname : 'sergio ramos'
+            }
+        },
+        methods: {
+            gonder(){
+                this.isOn = true;
+            }
+        },
+    }
+</script>
+
+<style>
+</style>
---
+++ BOLUM 2/odev-9/src/Fullname.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="form-group">
+      <label for>adı</label>
+      <input type="text" class="form-control" :value="name" @input="nameChanged(true, $event)"/>
+    </div>
+    <div class="form-group">
+      <label for>soyadı</label>
+      <input type="text" class="form-control" :value="surname" @input="nameChanged(false, $event)"/>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    props : ["value"],
+    computed : {
+        name(){
+            return this.value.split(' ')[0];
+        },
+        surname(){
+            return this.value.split(' ')[1]
+        }
+    },
+    methods: {
+        nameChanged(isFirst, event){
+            let fullName = '';
+            if(isFirst){
+                fullName = event.target.value + ' ' + this.surname
+            } else {
+                fullName = this.name + ' ' + event.target.value
+            }
+            this.value = fullName;
+            this.$emit("input", this.value)
+        }
+    }
+};
+</script>
+
+<style>
+</style>
