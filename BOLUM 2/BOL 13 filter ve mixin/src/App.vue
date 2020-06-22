0c04a135974a5dea8277ba10f3e3487bea9048a9
kebele <ykemalbeler@gmail.com>
2020-06-23 00:02:32 +0300

    ekleme

--- BOLUM 2/BOL 13 filter ve mixin/src/App.vue
+++ BOLUM 2/BOL 13 filter ve mixin/src/App.vue
@@ -3,17 +3,50 @@
     <div class="row">
       <div class="col-md-6 col-md-offset-3">
         <h3>Filter ve Mixins</h3>
-
+        <p>{{ message | toUppercase | toLowercase }}</p>
+        <hr />
+        <input type="text" v-model="searchText" placeholder="search"/>
+        <ul>
+          <!-- <li v-for="product in products" :key="product.id">{{ product }}</li> -->
+          <li v-for="product in filtered" :key="product.id">{{ product }}</li>
+        </ul>
+        <app-list></app-list>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import FilterList from './FilterList'
+import { productMixin } from './productMixin'
+
 export default {
-}
+  components : {
+    appList : FilterList
+  },
+  data() {
+    return {
+      message: "selamlar kablosuzkedi | videosinif",
+      // products: ["monitor", "klavye", "masa", "kitap", "şise", "hard disk"],
+      //mixin de olduğu için yoruma aldım
+      searchText: "",
+    };
+  },
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase();
+    },
+  },
+  //mixin de old. için yoruma aldım
+  // computed: {
+  //   filtered(){
+  //     return this.products.filter((element)=>{
+  //       return element.match(this.searchText);
+  //     })
+  //   }
+  // },
+  mixins : [productMixin]
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ BOLUM 2/BOL 13 filter ve mixin/src/FilterList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <hr />
+        <input type="text" v-model="searchText" placeholder="search"/>
+        <ul>         
+          <li v-for="product in filtered" :key="product.id">{{ product }}</li>
+        </ul>
+    </div>
+</template>
+
+
+<script>
+
+import { productMixin } from './productMixin'
+
+export default {
+  //mixin olarak kullanmak için aldık
+
+  // data() {
+  //   return {      
+  //     products: ["monitor", "klavye", "masa", "kitap", "şise", "hard disk"],
+  //     searchText: "",
+  //   };
+  // },
+  // computed: {
+  //   filtered(){
+  //     return this.products.filter((element)=>{
+  //       return element.match(this.searchText);
+  //     })
+  //   }
+  // },
+  mixins : [productMixin]
+};
+</script>
