00fa470f8152ad8df14412fa9449317545e88f31
kebele <ykemalbeler@gmail.com>
2020-06-24 06:59:47 +0300

    ekleme

---
+++ BOLUM 2/UYG 3 kedi nerede/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+</template>
+<script>
+
+import GameCards from "./components/GameCards"
+import Failure from "./components/Failure"
+import Celebrate from "./components/Celebrate"
+
+export default {
+    components : {
+        appGameCards : GameCards,
+        appCelebrate : Celebrate,
+        appFailure : Failure, 
+    }
+}
+</script>
+<style></style>
