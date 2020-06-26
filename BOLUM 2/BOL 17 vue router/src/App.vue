2a7c620fb1874fe64a84194691056adee933b6b7
kebele <ykemalbeler@gmail.com>
2020-06-26 23:07:16 +0300

    ekleme

--- BOLUM 2/BOL 17 vue router/src/App.vue
+++ BOLUM 2/BOL 17 vue router/src/App.vue
@@ -1,8 +1,17 @@
 <template>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <router-view></router-view>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
+
 export default {
+
 }
 </script>
 
--- BOLUM 2/BOL 17 vue router/src/main.js
+++ BOLUM 2/BOL 17 vue router/src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+import { routes } from './routes'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes : routes
+  //es6 da routes şeklinde tek yazarakda kullanabiliriz bunun anlamı routes : routes dir
+  //bunu projemizde kullanabilmek için instance ımız içinde yani new Vue içinde de belirlememiz lazım
+});
 
 new Vue({
   el: '#app',
+  router,
+  //burada da aynı şey tek router , aynı şekilde router : router demek
   render: h => h(App)
 })
+
