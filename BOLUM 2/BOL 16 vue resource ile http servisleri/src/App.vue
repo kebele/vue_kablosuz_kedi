04bd582a1d958f3797291c14a6ea61b5454afc8b
kebele <ykemalbeler@gmail.com>
2020-06-26 10:57:36 +0300

    ekleme

--- BOLUM 2/BOL 16 vue resource ile http servisleri/src/App.vue
+++ BOLUM 2/BOL 16 vue resource ile http servisleri/src/App.vue
@@ -15,6 +15,21 @@
           <button 
             class="btn btn-success"
             @click="getUsers">verileri getir</button>
+            <hr>
+            <ul class="list-group">
+              <!-- <li class="list-group-item">deneme</li> -->
+              <li 
+                class="list-group-item"
+                v-for="user in userList" 
+                :key="user.id">
+                <span>{{ user.data.userName }}</span>
+                  <button 
+                    class="btn btn-xs btn-danger"
+                    @click="deleteUser(user.key)"
+                  >sil
+                  </button>
+                </li>
+            </ul>
       </div>
     </div>
   </div>
@@ -25,6 +40,7 @@ export default {
   data(){
     return {
       userName : null,
+      userList : [],
     }
   },
   methods: {
@@ -33,20 +49,66 @@ export default {
       // this.$http.post(url, data)
       // this.$http.post("https://vue-resource-be59c.firebaseio.com/users/jack/name.json", data)
       //bu url ye bu veriyi post'la
-      this.$http.post("https://vue-resource-be59c.firebaseio.com/users.json", { userName : this.userName})
-        .then((response)=>{
-          console.log(response)
-        })
+      // this.$http.post("https://vue-resource-be59c.firebaseio.com/users.json", { userName : this.userName})
+
+      //root kullanımı, main e bak
+      // this.$http.post("users.json", {userName : this.userName})
+      //   .then((response)=>{
+      //     console.log(response)
+      //   })
+      //yukarıyı post a alternatif için yoruma aldım
+      // this.$resource("users.json").save({}, { userName : this.userName});
+      this.resource.kaydet({}, { userName : this.userName })
     },
     getUsers(){
+
+      this.$resource("users.json").get()
+
       // alert("123")
-      this.$http.get("https://vue-resource-be59c.firebaseio.com/users.json")
+      // this.$http.get("https://vue-resource-be59c.firebaseio.com/users.json")
+      // this.$http.get("users.json")
+      //bunu vue.resource u kullanabilmek için yoruma aldım
         .then((response)=>{
           // console.log(response);
-          console.log(response.body);
-        })
-    }
+          // console.log(response.body);
+          //aşağıyı main.js de next() değişikliği yaptığımız için yoruma aldım, 
+        //   let data = response.data;
+        //   for(let key in data){
+        //     // console.log(data[key])
+        //     this.userList.push(data[key]);
+        //   }
+        // })
+        return response.json()
+    }).then(data => {
+      console.log(data);
+      for(let key in data.userList){
+        // this.userList.push(data.userList[key])
+        //delete için firebase deki keyleride tutmamız lazım
+        this.userList.push(
+          {
+            key : key,
+            data : data.userList[key],
+          }
+        )
+      }
+    })
   },
+  deleteUser(userKey){
+    // alert("delete")
+    //vue resource da delete methodnu kullanmak için aşağıyı yoruma aldım
+    // this.$http.delete("users/" + userKey + ".json")
+    //   .then((response)=>{
+    //     console.log(response);
+    //   })
+    this.$resource.delete("users/" + userKey + ".json")
+  }
+},
+created(){
+  const customActions = {
+    kaydet : {method : "POST", url : "users.json"}
+  };
+  this.resource = this.$resource("", {}, customActions);
+}
 }
 </script>
 
--- BOLUM 2/BOL 16 vue resource ile http servisleri/src/main.js
+++ BOLUM 2/BOL 16 vue resource ile http servisleri/src/main.js
@@ -4,6 +4,25 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
+Vue.http.options.root = "https://vue-resource-be59c.firebaseio.com"
+
+Vue.http.interceptors.push((request, next)=>{
+  console.log(request.method)
+  //isteğimizin methodu GET/POST/... ?
+  //  if(request.method == "POST"){
+  //    request.method = "PUT"
+  //  }
+  //  next();
+  next((response)=>{
+    // console.log(response);
+    response.json = ()=>{
+      return {
+        userList : response.body
+      }
+    }
+  })
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
