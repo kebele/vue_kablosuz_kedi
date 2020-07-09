36ee235b098c4ca2d51fa9ce71599585c0141e2d
kebele <ykemalbeler@gmail.com>
2020-07-09 10:59:23 +0300

    ekleme

--- BOLUM 2/BOL 19 UYG 4 urun islemleri/src/components/products/ProductSell.vue
+++ BOLUM 2/BOL 19 UYG 4 urun islemleri/src/components/products/ProductSell.vue
@@ -1,72 +1,90 @@
 <template>
   <div>
-   <div class="container">
-    <div class="row">
+    <div class="container">
+      <div class="row">
         <div class="col-6 offset-3 pt-3 card mt-5 shadow">
-            <div class="card-body">
-                <h3>Ürün Çıkışı</h3>
-                <hr>
-                <div class="form-group">
-                    <label>Ürün Adı</label>
-                    <select class="form-control" 
-                        v-model="selectedProduct"
-                        @change="productSelected">
-                        <option 
-                            v-for="product in getProducts" 
-                            :key="product.id"
-                            :value="product.key"
-                            >{{ product.title }}
-                        </option>
-                        
-                    </select>
-                </div>
-                <div class="card mb-2 border border-danger">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-12 text-center">
-                                <div class="mb-3">
-                                    <span class="badge badge-info">Stok : 4</span>
-                                    <span class="badge badge-primary">Fiyat : 100,5 TL</span>
-                                </div>
-                                <p class="border border-warning p-2 text-secondary">Lorem ipsum dolor sit amet, consectetur
-                                    adipisicing elit. Assumenda debitis deleniti eos impedit iste numquam quos sit.
-                                    Dignissimos, mollitia nemo officia reiciendis repellendus rerum velit. Eos libero magnam
-                                    quas tempore!</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label>Adet</label>
-                    <input type="text" class="form-control" placeholder="Ürün adetini giriniz..">
-                </div>
-                <hr>
-                <button class="btn btn-primary">Kaydet</button>
+          <div class="card-body">
+            <h3>Ürün Çıkışı</h3>
+            <hr />
+            <div class="form-group">
+              <label>Ürün Adı</label>
+              <select
+                class="form-control"
+                v-model="selectedProduct"
+                @change="productSelected"
+              >
+              <option disabled>bir ürün seçiniz</option>
+                <option
+                    :disabled="product.count == 0"
+                  v-for="product in getProducts"
+                  :key="product.id"
+                  :value="product.key"
+                  >{{ product.title }}
+                </option>
+              </select>
             </div>
+
+            <transition name="fade" mode="out-in">
+              <div
+                class="card mb-2 border border-danger"
+                v-if="product !== null"
+              >
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-12 text-center">
+                      <div class="mb-3">
+                        <span class="badge badge-info"
+                          >Stok : {{ product.count }}</span
+                        >
+                        <span class="badge badge-primary"
+                          >Fiyat : {{ product.price | currency }}</span
+                        >
+                      </div>
+                      <p class="border border-warning p-2 text-secondary">
+                        {{ product.description }}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </transition>
+            <div class="form-group">
+              <label>Adet</label>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Ürün adetini giriniz.."
+              />
+            </div>
+            <hr />
+            <button class="btn btn-primary">Kaydet</button>
+          </div>
         </div>
+      </div>
     </div>
-</div>
   </div>
 </template>
 <script>
-import { mapGetters} from 'vuex'
+import { mapGetters } from "vuex";
 
 export default {
-    data(){
-        return {
-            selectedProduct : null,
-        }
+  data() {
+    return {
+      selectedProduct: null,
+      product: null,
+    };
+  },
+  computed: {
+    ...mapGetters(["getProducts"]),
+  },
+  methods: {
+    productSelected() {
+      console.log(this.selectedProduct);
+      // let product = this.$store.getters.getProduct(this.selectedProduct)
+      // console.log(product)
+      this.product = this.$store.getters.getProduct(this.selectedProduct)[0];
     },
-    computed : {
-        ...mapGetters(["getProducts"])
-    },
-    methods: {
-        productSelected(){
-            console.log(this.selectedProduct)
-            let product = this.$store.getters.getProduct(this.selectedProduct)
-            console.log(product)
-        }
-    },
+  },
 };
 </script>
 <style scoped>
