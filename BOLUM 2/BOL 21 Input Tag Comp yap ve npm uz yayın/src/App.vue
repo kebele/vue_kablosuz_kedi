caef073667f66d643ad29b3509a78d904aebeaaa
kebele <ykemalbeler@gmail.com>
2020-07-11 17:17:25 +0300

    ekleme

--- BOLUM 2/BOL 21 Input Tag Comp yap ve npm uz yayın/src/App.vue
+++ BOLUM 2/BOL 21 Input Tag Comp yap ve npm uz yayın/src/App.vue
@@ -1,60 +1,57 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="tag-container">
+    <span class="tag" v-for="tag in tags" :key="tag.id">
+      <span class="content">{{ tag }}</span>
+      <span class="close">X</span>
+    </span>
+    <!-- <input type="text" @keydown.enter="tags.push($event.target.value)"> -->
+    <input type="text" @keydown.enter="addTag">
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  data(){
     return {
-      msg: 'Welcome to Your Vue.js App'
+      tags : ["deneme", "test"],
     }
-  }
+  },
+  methods: {
+    addTag(event){
+      this.tags.push(event.target.value);
+      event.target.value = "";
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    font-family: sans-serif;
+  }
+  .tag-container{
+    border: 1px solid #ccc;
+    padding: 20px;
+  }
 
-h1, h2 {
-  font-weight: normal;
-}
+  input{
+    outline: none;
+    height: 30px;
+    width: 100px;
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  .tag{
+    background-color: #fbbb08;
+    padding: 10px;
+    color: #000;
+    cursor: default;
+    font-size: 14px;
+    margin-right: 10px;
+  }
 
-a {
-  color: #42b983;
-}
+  .tag .close {
+    font-size: 12px;
+    cursor: pointer;
+  }
 </style>
