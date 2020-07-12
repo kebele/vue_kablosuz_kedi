69acd5310c12c398e9f62a197a1fd0f2e421af16
kebele <ykemalbeler@gmail.com>
2020-07-13 00:39:35 +0300

    ekleme

--- BOLUM 2/BOL 22 vue resource yerine axios/src/components/NewPost.vue
+++ BOLUM 2/BOL 22 vue resource yerine axios/src/components/NewPost.vue
@@ -1,27 +1,52 @@
 <template>
   <div class="container pt-5">
     <h3>Yazı Ekle</h3>
-    <hr>
+    <hr />
     <form @submit.prevent="onSubmit">
       <div class="form-group">
         <label>Yazar</label>
-        <input v-model="post.author" type="text" class="form-control" placeholder="Yazar adını giriniz...">
+        <input
+          v-model="post.author"
+          type="text"
+          class="form-control"
+          placeholder="Yazar adını giriniz..."
+        />
       </div>
       <div class="form-group">
         <label>Başlık</label>
-        <input v-model="post.title" type="text" class="form-control" placeholder="Yazının başlığını giriniz...">
+        <input
+          v-model="post.title"
+          type="text"
+          class="form-control"
+          placeholder="Yazının başlığını giriniz..."
+        />
       </div>
       <div class="form-group">
         <label>Görsel Bağlantısı</label>
-        <input v-model="post.thumbnail" type="text" class="form-control" placeholder="Yazının görselini giriniz...">
+        <input
+          v-model="post.thumbnail"
+          type="text"
+          class="form-control"
+          placeholder="Yazının görselini giriniz..."
+        />
       </div>
       <div class="form-group">
         <label>Önizleme Metni</label>
-        <input v-model="post.previewText" type="text" class="form-control" placeholder="Yazının önizleme metnini giriniz...">
+        <input
+          v-model="post.previewText"
+          type="text"
+          class="form-control"
+          placeholder="Yazının önizleme metnini giriniz..."
+        />
       </div>
       <div class="form-group">
         <label>Yazı</label>
-        <textarea v-model="post.content" cols="30" rows="5" class="form-control"></textarea>
+        <textarea
+          v-model="post.content"
+          cols="30"
+          rows="5"
+          class="form-control"
+        ></textarea>
       </div>
       <button @click="$router.push('/')" class="btn btn-danger">İptal</button>
       <button type="submit" class="btn btn-info">Kaydet</button>
@@ -29,23 +54,31 @@
   </div>
 </template>
 <script>
-  export default {
-    data(){
-      return {
-        post : {
-          author : "",
-          content : "",
-          previewText : "",
-          thumbnail : "",
-          title : "",
-          updatedDate : ""
-        }
-      }
+import axios from "axios"
+
+export default {
+  data() {
+    return {
+      post: {
+        author: "",
+        content: "",
+        previewText: "",
+        thumbnail: "",
+        title: "",
+        updatedDate: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      axios.post("https://vuejs-axios-blog-78f95.firebaseio.com/posts.json", this.post, this.post.updatedDate = new Date())
+        .then(response => {
+          console.log(response)
+        })
+        .catch(e => console.log(e))
+      // console.log(this.post);
+      // console.log({...this.post, updatedDate : new Date()});
     },
-    methods : {
-      onSubmit(){
-        // console.log({...this.post, updatedDate : new Date()});
-      }
-    }
-  }
+  },
+};
 </script>
