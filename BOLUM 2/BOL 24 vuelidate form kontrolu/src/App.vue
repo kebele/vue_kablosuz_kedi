a7932f677e3d5c3f6b3e9875bdcd5f570cbd10b6
kebele <ykemalbeler@gmail.com>
2020-07-14 03:11:20 +0300

    ekleme

--- BOLUM 2/BOL 24 vuelidate form kontrolu/src/App.vue
+++ BOLUM 2/BOL 24 vuelidate form kontrolu/src/App.vue
@@ -6,71 +6,128 @@
         <form style="width: 350px" @submit.prevent="onSubmit">
           <div class="form-group">
             <label>E-posta Adresiniz</label>
-            <input v-model="email" type="email" class="form-control" placeholder="E-posta adresini giriniz">
+            <input
+              v-model="email"
+              type="email"
+              class="form-control"
+              placeholder="E-posta adresini giriniz"
+            />
             <small class="form-text text-muted">Hata Mesajı</small>
           </div>
           <div class="form-group">
             <label>Şifre</label>
-            <input v-model="password" type="password" class="form-control" placeholder="Şifrenizi giriniz">
+            <input
+              v-model="password"
+              type="password"
+              class="form-control"
+              placeholder="Şifrenizi giriniz"
+            />
           </div>
           <div class="form-group">
             <label>Şifre Tekrar</label>
-            <input v-model="repassword" type="password" class="form-control" placeholder="Şifrenizi tekrar giriniz">
+            <input
+              v-model="repassword"
+              type="password"
+              class="form-control"
+              placeholder="Şifrenizi tekrar giriniz"
+            />
           </div>
           <div class="form-group">
             <label>Kayıt olmak istediğiniz kategori</label>
             <select v-model="selectedCategory" class="form-control">
-              <option v-for="category in categories" :key="category.id">{{ category }}</option>
+              <option v-for="category in categories" :key="category.id">{{
+                category
+              }}</option>
             </select>
           </div>
 
-          <a @click="newHobby" class="text-white btn btn-secondary rounded-0 btn-sm">İlgi Alanı Ekle</a>
+          <a
+            @click="newHobby"
+            class="text-white btn btn-secondary rounded-0 btn-sm"
+            >İlgi Alanı Ekle</a
+          >
 
           <ul class="list-group mt-3 mb-3 border-0">
-            <li v-for="(hobby,index) in hobbies" :key="hobby.index" class="list-group-item d-flex pl-2">
-              <input type="text" class="form-control mr-2" v-model="hobby.value">
-              <button class="btn btn-sm btn-danger rounded-0" @click="hobbies.splice(index, 1)">Sil</button>
+            <li
+              v-for="(hobby, index) in hobbies"
+              :key="hobby.index"
+              class="list-group-item d-flex pl-2"
+            >
+              <input
+                type="text"
+                class="form-control mr-2"
+                v-model="hobby.value"
+              />
+              <button
+                class="btn btn-sm btn-danger rounded-0"
+                @click="hobbies.splice(index, 1)"
+              >
+                Sil
+              </button>
             </li>
           </ul>
-         
+
           <button class="btn btn-outline-secondary rounded-0">Kaydet</button>
         </form>
       </div>
       <div class="card p-4 mt-3 shadow" style="width: 400px">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod consectetur vero fugiat nobis aliquam quidem distinctio aspernatur aliquid odio porro itaque ad repellat, veritatis omnis eius ab nisi rem ea architecto odit mollitia, suscipit harum possimus! Voluptates aliquid aperiam animi dolor, dolore, suscipit hic laudantium reiciendis consequuntur, non omnis impedit!</p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod
+          consectetur vero fugiat nobis aliquam quidem distinctio aspernatur
+          aliquid odio porro itaque ad repellat, veritatis omnis eius ab nisi
+          rem ea architecto odit mollitia, suscipit harum possimus! Voluptates
+          aliquid aperiam animi dolor, dolore, suscipit hic laudantium
+          reiciendis consequuntur, non omnis impedit!
+        </p>
       </div>
     </div>
   </div>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        email : null,
-        password : null,
-        repassword : null,
-        selectedCategory : null,
-        categories : ["Yazılım", "Donanım", "Cloud", "Sunucular", "Unix", "Linux", "Mac OS", "Windows"],
-        hobbies: []
-      }
+import { required } from "vuelidate/lib/validators";
+
+export default {
+  data() {
+    return {
+      email: null,
+      password: null,
+      repassword: null,
+      selectedCategory: null,
+      categories: [
+        "Yazılım",
+        "Donanım",
+        "Cloud",
+        "Sunucular",
+        "Unix",
+        "Linux",
+        "Mac OS",
+        "Windows",
+      ],
+      hobbies: [],
+    };
+  },
+  validations: {
+    email: {
+      required: required,
     },
-    methods: {
-      onSubmit(){
-        let form = {
-          email : this.email,
-          password : this.password,
-          category : this.category,
-          hobbies : this.hobbies,
-        }
-        console.log(form)
-      },
-      newHobby(){
-        let hobby = {
-          id: Math.random() * Math.random() * 1000,
-          value: ''
-        }
-        this.hobbies.push(hobby)
-      }
-    }
-  }
+  },
+  methods: {
+    onSubmit() {
+      let form = {
+        email: this.email,
+        password: this.password,
+        category: this.category,
+        hobbies: this.hobbies,
+      };
+      console.log(form);
+    },
+    newHobby() {
+      let hobby = {
+        id: Math.random() * Math.random() * 1000,
+        value: "",
+      };
+      this.hobbies.push(hobby);
+    },
+  },
+};
 </script>
