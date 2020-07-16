ae45edd6c66b191420a10270c9dc8a55a3d43cdd
kebele <ykemalbeler@gmail.com>
2020-07-16 10:35:17 +0300

    ekleme

--- BOLUM 2/BOL 25 vue auth/src/store.js
+++ BOLUM 2/BOL 25 vue auth/src/store.js
@@ -4,10 +4,28 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-    state : {},
-    getters : {},
-    mutations : {},
-    actions : {}
+    state : {
+        token : ""
+    },
+    mutations : {
+        setToken(state, token){
+            state.token = token
+        },
+        clearToken(state){
+            state.token = ""
+        }
+    },
+    actions : {
+        login({ commit, dispatch, state }, authData){
+
+        },
+        logout({ commit, dispatch, state }){
+
+        }
+    },
+    getters : {
+        
+    },
 })
 
 export default store
